c6add52415820d682313abb83636ce1a0d509301
Nathaniel Truitt <ntruitt@Nathaniels-MacBook-Pro.local>
2022-10-03 13:56:34 -0500

    adding components

---
+++ src/app/order-dashboard/all-other-orders/all-other-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-other-orders',
+  templateUrl: './all-other-orders.component.html',
+  styleUrls: ['./all-other-orders.component.css']
+})
+export class AllOtherOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/order-dashboard/first-five-orders/first-five-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-five-orders',
+  templateUrl: './first-five-orders.component.html',
+  styleUrls: ['./first-five-orders.component.css']
+})
+export class FirstFiveOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
