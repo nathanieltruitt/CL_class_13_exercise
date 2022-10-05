4fcc7b0e550af83211afa089e3e91641a5bf26b5
Nathaniel Truitt <ntruitt@Nathaniels-MacBook-Pro.local>
2022-10-05 17:16:41 -0500

    added second exercise, removed test files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FirstFiveOrdersComponent } from './order-dashboard/first-five-orders/fi
 import { AllOtherOrdersComponent } from './order-dashboard/all-other-orders/all-other-orders.component';
 import { LotteryWinningOrderComponent } from './order-dashboard/lottery-winning-order/lottery-winning-order.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BasicCustomPropertyExerciseComponent } from './basic-custom-property-exercise/basic-custom-property-exercise.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     AllOtherOrdersComponent,
     LotteryWinningOrderComponent,
     NavbarComponent,
+    BasicCustomPropertyExerciseComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/basic-custom-property-exercise/basic-custom-property-exercise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-custom-property-exercise',
+  templateUrl: './basic-custom-property-exercise.component.html',
+  styleUrls: ['./basic-custom-property-exercise.component.css']
+})
+export class BasicCustomPropertyExerciseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
